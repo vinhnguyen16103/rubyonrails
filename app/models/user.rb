f1e12397a08501792a6b646833280b21c207bd25
vinhnguyen16103 <nguyenvinhnguyen03@gmail.com>
2025-01-06 22:59:17 +0700

    update

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :sessions, dependent: :destroy
-
-  normalizes :email_address, with: ->(e) { e.strip.downcase }
+  validates :email_address, presence: true, if: Proc.new { |user| user.approved? }
+  # normalizes :email_address, with: ->(e) { e.strip.downcase }
 end
