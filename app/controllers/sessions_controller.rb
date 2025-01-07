519e0a8275aa73f14aa791efda98d0d987fc44a4
vinhnguyen16103 <nguyenvinhnguyen03@gmail.com>
2025-01-07 22:38:01 +0700

    update

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,12 +3,11 @@ class SessionsController < ApplicationController
   rate_limit to: 10, within: 3.minutes, only: :create, with: -> { redirect_to new_session_url, alert: "Try again later." }
 
   def new
-    unless User.exists?('nguyennophake102003@gmail.com')
-      User.create!(
-        email_address:'nguyennophake102003@gmail.com',
-        password:'nguyendeptrai',
-        password_confirmation:'nguyendeptrai'
-      )
+    unless User.exists?(email_address:'nguyennofake@gmail.com')
+      User.create!(email_address:'nguyennofake@gmail.com',
+      password:'nguyendeptrai',
+      password_confirmation:'nguyendeptrai')
+
     end
   end
 
--- app/models/product.rb
+++ app/models/product.rb
@@ -7,16 +7,16 @@ class Product < ApplicationRecord
     validates :name, presence: true
     validates :inventory_count, numericality: { greater_than_or_equal_to: 0 }
   
-    after_update_commit :notify_subscribers, if: :back_in_stock?
-  
-    def back_in_stock?
-      inventory_count_previously_was.zero? && inventory_count > 0
-    end
-  
-    def notify_subscribers
-      subscribers.each do |subscriber|
-        ProductMailer.with(product: self, subscriber: subscriber).in_stock.deliver_later
-      end
+     after_update_commit :notify_subscribers, if: :back_in_stock?
+
+  def back_in_stock?
+    inventory_count_previously_was.zero? && inventory_count > 0
+  end
+
+  def notify_subscribers
+    subscribers.each do |subscriber|
+      ProductMailer.with(product: self, subscriber: subscriber).in_stock.deliver_later
     end
   end
+end
   
